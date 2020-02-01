25901f840a282c0eb5f0c2aee684c3a6f8ceffb7
William Tumeo <tumeowilliam@gmail.com>
2020-02-01 17:30:49 -0300

    Add release check for item

--- game/gameplay/grab-items/Piece.gd
+++ game/gameplay/grab-items/Piece.gd
@@ -3,11 +3,13 @@ extends GrabItem
 
 
 export(PoolIntArray) var dependencies
+var glue = 0
 
+func has_glue():
+	return glue > 0
 
 func get_id():
 	return int($Sprite.animation)
-
 
 func release_sprite():
 	var sprite = $Sprite
