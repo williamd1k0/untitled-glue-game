3025e83ba369fba70e5a86bc7c671f78e9d086d8
William Tumeo <tumeowilliam@gmail.com>
2020-02-01 02:10:35 -0300

    Add Glue

--- game/gameplay/GrabItem.gd
+++ game/gameplay/GrabItem.gd
@@ -10,17 +10,16 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	translate(Vector2(0, 0))
+	#translate(Vector2(0, 0))
 	pass
 
 func _integrate_forces(state):
 	if grabbed != null:
 		state.transform = grabbed.global_transform
-		print(grabbed.global_transform)
 
 func grab(hand):
-	collision_mask = 0
-	collision_layer = 0
+	#collision_mask = 0
+	#collision_layer = 0
 	gravity_scale = 0
 	grabbed = hand
 	rotation = 0
@@ -28,7 +27,7 @@ func grab(hand):
 
 func release():
 	grabbed = null
-	collision_mask = 1
-	collision_layer = 1
+	#collision_mask = 1
+	#collision_layer = 1
 	gravity_scale = 1
 	$Animator.play("idle")
