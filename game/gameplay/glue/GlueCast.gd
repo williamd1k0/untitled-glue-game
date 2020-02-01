92ebbc01a3bfd4303e6ce3e0b80fea7a3c9e3553
William Tumeo <tumeowilliam@gmail.com>
2020-02-01 20:05:25 -0300

    Fix hand grab behavior and fix glue cast

--- game/gameplay/glue/GlueCast.gd
+++ game/gameplay/glue/GlueCast.gd
@@ -3,12 +3,6 @@ extends RayCast2D
 export(float) var glue_amount = 0.2
 
 
-func start():
-	enabled = true
-
-func stop():
-	enabled = false
-
 func _physics_process(delta):
 	if enabled and is_colliding():
 		var piece: Piece = get_collider()
