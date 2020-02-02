892d0584c8bd1198122dbac925fac9459ef2e012
Henrique Campos <henriquecampos@pigdev.studio>
2020-02-02 03:15:14 -0300

    Fix BGM not disabling effect

--- game/BGM.gd
+++ game/BGM.gd
@@ -8,8 +8,28 @@ func _ready():
 	bus_index = AudioServer.get_bus_index(bus)
 	
 func start_effect(effect):
+	if is_effect_enabled(effect):
+		return
 	$AnimationPlayer.play(effect)
+
+
+func stop_effect(effect):
+	$AnimationPlayer.stop()
+	enable_effect(effect, false)
+
+
+func reverse_effect(effect):
+	if not is_effect_enabled(effect):
+		return
+	$AnimationPlayer.play_backwards(effect)
+	yield($AnimationPlayer, "animation_finished")
+	stop_effect(effect)
 
 
 func enable_effect(effect_name, enable):
 	AudioServer.set_bus_effect_enabled(bus_index, Effects[effect_name], enable)
+
+
+func is_effect_enabled(effect_name):
+	var enabled = AudioServer.is_bus_effect_enabled(bus_index, Effects[effect_name])
+	return enabled
--- game/Game.gd
+++ game/Game.gd
@@ -7,6 +7,7 @@ var score = 0 setget set_score
 
 func _ready():
 	set_score(0)
+	BGM.reverse_effect("high_pass")
 
 
 func set_score(value):
@@ -20,7 +21,13 @@ func _on_Puzzle_piece_fit(piece_position, fit_score):
 
 
 func _on_Time_finished():
-	BGM.start_effect("game_over")
+	BGM.start_effect("high_pass")
 	game_over_screen.set_final_score(score)
 	game_over_screen.show()
 	get_tree().paused = true
+
+
+func _on_Puzzle_tree_exited():
+	var puzzle = $LevelLayer/Level/PuzzleSpawner.spawn()
+	puzzle.connect("tree_exited", self, "_on_Puzzle_tree_exited")
+	puzzle.connect("piece_fit", self, "_on_Puzzle_piece_fit")
