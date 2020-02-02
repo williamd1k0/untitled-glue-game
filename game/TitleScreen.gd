66823b5bce0bcbfe7609f25ffed811d5c9509114
Henrique Campos <henriquecampos@pigdev.studio>
2020-02-02 16:03:28 -0300

    Add Credits screen

--- game/TitleScreen.gd
+++ game/TitleScreen.gd
@@ -1,12 +1,18 @@
 extends Control
 
 export(String, FILE, '*.tscn') var game_scene
+export(String, FILE, '*.tscn') var credits_scene
 
 func _ready():
 	$Menu/Play.grab_focus()
 
 func _on_Play_pressed():
 	get_tree().change_scene(game_scene)
+
+
+func _on_Credits_pressed():
+	get_tree().change_scene(credits_scene)
+
 
 func _on_Quit_pressed():
 	get_tree().quit()
