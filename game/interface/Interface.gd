12e51bb46a7041f4627196a8ea204b75bca927fd
Henrique Campos <henriquecampos@pigdev.studio>
2020-02-01 23:21:14 -0300

    Remove UI from TestGrab

--- game/interface/Interface.gd
+++ game/interface/Interface.gd
@@ -2,6 +2,7 @@ extends Control
 
 export (Resource) var glue_charge
 export (Resource) var time
+export (PackedScene) var pop_label_scene
 
 onready var glue_bar = $GlueAmountBar
 onready var clock = $TimeProgressBar
@@ -22,3 +23,10 @@ func _on_Glue_amount_changed(amount):
 
 func _on_Time_changed(value):
 	clock.value = value
+
+
+func _on_Puzzle_piece_fit(piece_position, piece_score):
+	var pop_label = pop_label_scene.instance()
+	pop_label.text = "%s" % piece_score
+	add_child(pop_label)
+	pop_label.rect_position = piece_position
