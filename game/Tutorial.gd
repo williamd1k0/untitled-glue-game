434b4e777746cf9dab12806db2dd91efba998e4a
Henrique Campos <henriquecampos@pigdev.studio>
2020-02-02 15:13:15 -0300

    Add tutorial

---
+++ game/Tutorial.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+onready var tutorial_piece = $LevelLayer/Level/Puzzle/Pieces/Piece3
+onready var piece_reference = $TutorialPieceReference
+
+func _ready():
+	tutorial_piece.global_position = piece_reference.global_position
+
+
+func _on_Puzzle_tree_exited():
+	get_tree().paused = true
+	$TutorialLayer/Instructions/Congratulations.show()
+	$TutorialLayer/Instructions/HBoxContainer.hide()
+	$TutorialLayer/Instructions/Congratulations/PlayButton.grab_focus()
+
+
+func _on_PlayButton_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://game/Game.tscn")
